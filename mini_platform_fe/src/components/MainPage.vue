226d971c360ba6c4198ad98ccb550d68ca39571e
ever53339 <caoxuan8872@gmail.com>
2024-10-10 17:32:28 -0700

    initial commit

---
+++ mini_platform_fe/src/components/MainPage.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="main">
+        <h1>Basic Control</h1>
+        <button>Start</button>
+        
+        <br>
+        
+        <button>Stop</button>
+        
+        <br>
+        Current Status
+    </div>
+</template>
+
+<script lang="ts">
+    export default{
+        name: "MainPage"
+    }
+</script>
+
+<style>
+    .main {
+        background-color: #3c68df;
+        box-shadow: 0 0 10px;
+        border-radius: 10px;
+        padding: 20px;
+    }
+
+</style>
